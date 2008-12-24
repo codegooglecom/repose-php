26339de63e025610c181447ce139710e8a888205
simensen <simensen@5653c02c-ccc2-11dd-a405-31afbefa9e0e>
2008-12-24 07:09:46 +0000

    o Minor updates to bootstrap file.

--- lib/repose.php
+++ lib/repose.php
@@ -4,7 +4,8 @@ if ( ! defined('REPOSE_LIB') ) {
     define('REPOSE_LIB', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 }
 
-// We only expose Session Factory 
 require_once(REPOSE_LIB . 'repose_Configuration.php');
+require_once(REPOSE_LIB . 'repose_SessionFactory.php');
+require_once(REPOSE_LIB . 'repose_Session.php');
 
 ?>
