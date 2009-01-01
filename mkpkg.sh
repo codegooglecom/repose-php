5049c07a603d733fc4aa9141dea8be07472959f3
simensen <simensen@5653c02c-ccc2-11dd-a405-31afbefa9e0e>
2009-01-01 12:29:24 +0000

    o Merged changes from repose-0.1 branch at r32 to repose-0.1-s1 tag.

--- mkpkg.sh
+++ mkpkg.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 TMP_PKG_BASE=/tmp/repose-$$
-TMP_PKG=${TMP_PKG_BASE}/repose-php
+
+if ! test -e .googlecodeauth
+then
+    echo "Google Code auth information required."
+    echo -n 'Username: ';
+    read USERNAME
+    echo -n 'Password: ';
+    read PASSWORD
+    echo "$USERNAME:$PASSWORD" > .googlecodeauth
+    chmod 600 .googlecodeauth
+fi
 
 if test "$#" = "2"
 then
@@ -12,20 +22,21 @@ then
         REPOSE_VISUAL_VERSION="Repose PHP ORM $2"
     else
         REPOSE_SVN_VERSION=branches/repose-$2
-        REPOSE_VISUL_VERSION=repose-${2}-snapshot-`date +%Y%m%d%H%M%S`
+        REPOSE_FILENAME_VERSION=repose-${2}-snapshot-`date +%Y%m%d%H%M%S`
         REPOSE_VISUAL_VERSION="Repose PHP ORM $2 Snapshot"
     fi
 else
     REPOSE_SVN_VERSION=trunk
-    REPOSE_FILENAME_VERSION=0.1-snapshot-`date +%Y%m%d%H%M%S`
-    REPOSE_VISUAL_VERSION=0.1-snapshot-`date +%Y%m%d%H%M%S`
+    REPOSE_FILENAME_VERSION=repose-snapshot-`date +%Y%m%d%H%M%S`
     REPOSE_VISUAL_VERSION="Repose PHP ORM Trunk Snapshot"
 fi
+
+TMP_PKG=${TMP_PKG_BASE}/${REPOSE_FILENAME_VERSION}
 
 REPOSE_LABELS=Type-Archive,OpSys-All
 
 PACKAGE_BASE=`pwd`/packaged
-PACKAGE_NAME_BASE=${PACKAGE_BASE}/repose-php-${REPOSE_FILENAME_VERSION}
+PACKAGE_NAME_BASE=${PACKAGE_BASE}/${REPOSE_FILENAME_VERSION}
 
 mkdir -p ${PACKAGE_BASE} >/dev/null 2>&1
 
@@ -34,9 +45,9 @@ mkdir -p ${TMP_PKG_BASE} >/dev/null 2>&1
 
 svn export http://repose-php.googlecode.com/svn/${REPOSE_SVN_VERSION}/lib ${TMP_PKG} >/dev/null 2>&1
 
-( cd ${TMP_PKG_BASE}; tar czf ${PACKAGE_NAME_BASE}.tar.gz repose-php/ ) >/dev/null 2>&1
-( cd ${TMP_PKG_BASE}; tar cjf ${PACKAGE_NAME_BASE}.tar.bz repose-php/ ) >/dev/null 2>&1
-( cd ${TMP_PKG_BASE}; zip ${PACKAGE_NAME_BASE}.zip repose-php/ ) >/dev/null 2>&1
+( cd ${TMP_PKG_BASE}; tar czf ${PACKAGE_NAME_BASE}.tar.gz ${REPOSE_FILENAME_VERSION} ) >/dev/null 2>&1
+( cd ${TMP_PKG_BASE}; tar cjf ${PACKAGE_NAME_BASE}.tar.bz ${REPOSE_FILENAME_VERSION} ) >/dev/null 2>&1
+( cd ${TMP_PKG_BASE}; zip ${PACKAGE_NAME_BASE}.zip ${REPOSE_FILENAME_VERSION} ) >/dev/null 2>&1
 
 
 rm -rf ${TMP_PKG_BASE} >/dev/null 2>&1
@@ -44,21 +55,21 @@ mkdir -p ${TMP_PKG_BASE} >/dev/null 2>&1
 
 svn export http://repose-php.googlecode.com/svn/${REPOSE_SVN_VERSION} ${TMP_PKG} >/dev/null 2>&1
 
-( cd ${TMP_PKG_BASE}; tar czf ${PACKAGE_NAME_BASE}-full.tar.gz repose-php/ ) >/dev/null 2>&1
-( cd ${TMP_PKG_BASE}; tar cjf ${PACKAGE_NAME_BASE}-full.tar.bz repose-php/ ) >/dev/null 2>&1
-( cd ${TMP_PKG_BASE}; zip ${PACKAGE_NAME_BASE}-full.zip repose-php/ ) >/dev/null 2>&1
+( cd ${TMP_PKG_BASE}; tar czf ${PACKAGE_NAME_BASE}-full.tar.gz ${REPOSE_FILENAME_VERSION} ) >/dev/null 2>&1
+( cd ${TMP_PKG_BASE}; tar cjf ${PACKAGE_NAME_BASE}-full.tar.bz ${REPOSE_FILENAME_VERSION} ) >/dev/null 2>&1
+( cd ${TMP_PKG_BASE}; zip ${PACKAGE_NAME_BASE}-full.zip ${REPOSE_FILENAME_VERSION} ) >/dev/null 2>&1
 
 rm -rf ${TMP_PKG_BASE} >/dev/null 2>&1
 
-for i in ${PACKAGE_NAME_BASE}.tar.gz ${PACKAGE_NAME_BASE}.tar.bz ${PACKAGE_NAME_BASE}.zip
+for i in ${PACKAGE_NAME_BASE}.tar.gz ${PACKAGE_NAME_BASE}.zip
 do
-    echo "googlecode_upload.py -s '${REPOSE_VISUAL_VERSION} (Libraries Only)' -p repose-php -l ${REPOSE_LABELS} $i"
+    ./googlecode_upload.php repose-php $i "${REPOSE_VISUAL_VERSION} (Libraries Only)" "${REPOSE_LABELS}"
 done
 
 
-for i in ${PACKAGE_NAME_BASE}-full.tar.gz ${PACKAGE_NAME_BASE}-full.tar.bz ${PACKAGE_NAME_BASE}-full.zip
+for i in ${PACKAGE_NAME_BASE}-full.tar.gz ${PACKAGE_NAME_BASE}-full.zip
 do
-    echo "googlecode_upload.py -s '${REPOSE_VISUAL_VERSION} (Full Package)' -p repose-php -l ${REPOSE_LABELS} $i"
+    ./googlecode_upload.php repose-php $i "${REPOSE_VISUAL_VERSION} (Full Package)" "${REPOSE_LABELS}"
 done
 
 
