e4627519b632bb5444630eb4bc538471ffcea39d
simensen <simensen@5653c02c-ccc2-11dd-a405-31afbefa9e0e>
2009-01-03 01:59:54 +0000

    o Fixed bug where array_unique was not working on objects (since it apparently converts them to a string). None of the unit tests apparently returned more than one result ever so this bug had not been detected.

--- tests/ReposeBasicTest.php
+++ tests/ReposeBasicTest.php
@@ -334,6 +334,11 @@ CREATE TABLE bug (
 
     }
 
+    public function testMultipleObjectsReturnedByQuery() {
+        $users = $this->getSession()->createQuery('FROM sample_User user')->execute();
+        $this->assertEquals(4, count($users));
+    }
+
 }
 
 ?>
